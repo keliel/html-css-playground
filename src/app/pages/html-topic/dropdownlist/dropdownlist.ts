09f5d5067049af7bd08bd4dd1eca67505af656b6
Hannah <5556710+keliel@users.noreply.github.com>
2026-03-03 14:53:11 +0900

    feat(html): add basic dropdownlist feature example

---
+++ src/app/pages/html-topic/dropdownlist/dropdownlist.ts
@@ -0,0 +1,52 @@
+import { Component, computed, signal } from '@angular/core';
+
+@Component({
+  selector: 'pg-dropdownlist',
+  imports: [],
+  templateUrl: './dropdownlist.html',
+  styleUrl: './dropdownlist.css',
+})
+export class DropdownlistComponent {
+
+  protected options = signal([{
+    text: 'Item One',
+    category: 'Category One'
+  }, {
+    text: 'Item Two',
+    category: 'Category Two'
+  }, {
+    text: 'Item Three',
+    category: 'Category Two'
+  }, {
+    text: 'Item Four',
+    category: 'Category Three'
+  }, {
+    text: 'Item Five',
+    category: 'Category Three'
+  }, {
+    text: 'Item Six',
+    category: 'Category Three'
+  }, {
+    text: 'Item Seven',
+    category: 'Category Four'
+  }, {
+    text: 'Item Eight',
+    category: 'Category Four'
+  }, {
+    text: 'Item Nine',
+    category: 'Category Four'
+  }, {
+    text: 'Item Ten',
+    category: 'Category Four'
+  }]);
+
+  protected groupedOptions = computed(() => this.options().reduce<{ category: string; options: ReturnType<typeof this.options> }[]>((g, o) => {
+    const group = g.find(g => g.category === o.category);
+    if (group) {
+      group.options.push(o);
+    } else {
+      g.push({ category: o.category, options: [o] });
+    }
+    return g;
+  }, []));
+}
--- src/app/pages/html-topic/html-topic.component.ts
+++ src/app/pages/html-topic/html-topic.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AccordionComponent } from './accordion/accordion.component';
 import { PopoverTooltipComponent } from './popover-tooltip/popover-tooltip.component';
+import { DropdownlistComponent } from "./dropdownlist/dropdownlist";
 
 @Component({
   selector: 'pg-html-topic',
-  imports: [AccordionComponent, PopoverTooltipComponent],
+  imports: [AccordionComponent, PopoverTooltipComponent, DropdownlistComponent],
   templateUrl: './html-topic.component.html',
   styleUrl: './html-topic.component.css',
 })
